c8152e7fbe3300d046fc54aa064f0512d6cde8a9
sh7478 <sh7478@bs.amalnet.k12.il>
2026-03-13 17:21:21 +0200

    added a function that reads all the text in the raw file

--- app/src/main/java/com/example/ex11024/MainActivity.java
+++ app/src/main/java/com/example/ex11024/MainActivity.java
@@ -8,11 +8,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
+    private final String FILENAME = "questions.txt";
+    String txt = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readFile();
+    }
+
+    private void readFile() {
+
+        String fileName = FILENAME.substring(0, FILENAME.length() - 4);
+        int resourceId = this.getResources().getIdentifier(fileName,
+                "raw", this.getPackageName());
+        try {
+        InputStream iS = this.getResources().openRawResource(resourceId);
+        InputStreamReader iSR = new InputStreamReader(iS);
+        BufferedReader bR = new BufferedReader(iSR);
+        StringBuilder sB = new StringBuilder();
+        String line = bR.readLine();
+        while(line != null)
+        {
+            sB.append(line+'\n');
+            line = bR.readLine();
+        }
+        txt = sB.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
