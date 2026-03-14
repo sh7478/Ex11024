9b404571bcf56f330a65e8cf75bf4fbe90521b4f
sh7478 <sh7478@bs.amalnet.k12.il>
2026-03-14 11:58:36 +0200

    added Question class

---
+++ app/src/main/java/com/example/ex11024/Question.java
@@ -0,0 +1,37 @@
+package com.example.ex11024;
+
+public class Question {
+    private String question;
+    private String[] answers;
+    private int correctAnswerIndex;
+
+    public Question(String question, String[] answers, int correctAnswerIndex) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(String[] answers) {
+        this.answers = answers;
+    }
+
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+
+    public void setCorrectAnswerIndex(int correctAnswerIndex) {
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+}
